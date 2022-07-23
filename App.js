7f74bc2d5dccf4bfcba0fb2862f86c7c472a36b6
F1NH4WK <araujoplayer@gmail.com>
2022-07-23 14:40:55 -0300

    Adding calculator buttons

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AwesomeButton from 'react-native-really-awesome-button';
+import { Ionicons } from '@expo/vector-icons'; 
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
+      <View style = {styles.screen}>
+        <Text style = {{color: 'white', fontSize: 30}}>202020</Text>
+      </View>
+      <View style = {styles.calculatorGroup}>
+        <View style = {styles.calculatorStyle}>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'black'}>C</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'black'}>+/-</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'black'}>%</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#fd6680'}>/</AwesomeButton>
+        </View>
+        <View style = {styles.calculatorStyle}>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}>7</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}>8</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}>9</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#fd6680'} textColor = {'white'}>x</AwesomeButton>
+        </View>
+        <View style = {styles.calculatorStyle}>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}>4</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}>5</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}>6</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#fd6680'}>-</AwesomeButton>
+        </View>
+        <View style = {styles.calculatorStyle}>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}>1</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}>2</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}>3</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#fd6680'}>+</AwesomeButton>
+        </View>
+        <View style = {styles.calculatorStyle}>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}>.</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}>0</AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#504c4f'} textColor = {'white'}><Ionicons name='backspace' size={23} color = 'white'/></AwesomeButton>
+          <AwesomeButton borderRadius={18} width={63} textSize = {23} raiseLevel = {0.1} backgroundColor = {'#fd6680'}>=</AwesomeButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#272529',
+    alignItems: 'center',
+    // color: #272529, #504c4f, #fd6680
+  },
+  screen: {
+    height: '30%',
+    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    // borderWidth: 2,
+    // borderColor: 'white',
+    padding: 15,
+    marginBottom: 20
+  },
+  calculatorStyle: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    // borderWidth: 2,
+    // borderColor: 'white',
+    height: '18%',
+    alignItems: 'center',
+    padding: 5
+    
+  },
+
+  calculatorGroup:{
+    flex: 1,
+    borderWidth: 2,
+    borderColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    padding: 20,
+  }
 });
